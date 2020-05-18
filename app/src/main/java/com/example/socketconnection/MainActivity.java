48a1d9b6ad2fb172710597177c4db9901bc1ec05
cf17josetorres <cf17jose.torres@iesjoandaustria.org>
2020-05-18 18:56:36 +0200

    Arreglos en en layout Intentando la conexion

--- app/src/main/java/com/example/socketconnection/MainActivity.java
+++ app/src/main/java/com/example/socketconnection/MainActivity.java
@@ -2,13 +2,76 @@ package com.example.socketconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText IPUsuario;
+    private Button CONECTAR, DESCONECTAR;
+    private static final int PORT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IPUsuario = findViewById(R.id.IPUsuario);
+        CONECTAR = findViewById(R.id.conexionaconectar);
+        DESCONECTAR =findViewById(R.id.conexiondesconectar);
+
+        CONECTAR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conexion conectar = new conexion();
+                conectar.execute();
+
+            }
+        });
+
+
+        DESCONECTAR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conexiondesc desconectar = new conexiondesc();
+                desconectar.execute();
+            }
+        });
     }
+
+    class conexion extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            String resultado;
+            try {
+                Socket socket = new Socket(strings[0], PORT);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    class conexiondesc extends  AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            return null;
+        }
+    }
+
+    /*@Override
+    protected void onPostExecute(Void mensaje) {
+        super.onPostExecute(mensaje);
+    }*/
 }
